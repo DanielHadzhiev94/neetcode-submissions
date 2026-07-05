8b7987be09dfc8c643a4cf95b48e6e81c1f95ad0
Daniel Hadzhiev <145060639+DanielHadzhiev94@users.noreply.github.com>
2026-07-05 23:48:13 +0200

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cpp
@@ -0,0 +1,25 @@
+
+class Solution {
+   public:
+    bool isPalindrome(string s) {
+        std::vector<char> text;
+
+        for (char c : s) {
+            if (std::isalnum(c)) {
+                text.push_back(std::tolower(c));
+            }
+        }
+
+        int n = text.size();
+        int it = 0;
+
+        while (it < n / 2) {
+            if (text[it] != text[n - it - 1]) {
+                return false;
+            }
+            ++it;
+        }
+
+        return true;
+    }
+};
