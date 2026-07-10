d047563ead67346ecb03d7d604d6ade3410bf2e3
Daniel Hadzhiev <145060639+DanielHadzhiev94@users.noreply.github.com>
2026-07-11 00:05:40 +0200

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.cpp
@@ -0,0 +1,31 @@
+class Solution {
+   public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        std::vector<int> result;
+        int n = temperatures.size() - 1;
+        for (int i = 0; i < n; ++i) {
+            bool warmer = false;
+            int temp = temperatures[i];
+            int counter = 0;
+            int it = i + 1;
+            while (it <= n) {
+                if (temperatures[it] > temp) {
+                    warmer = true;
+                    ++counter;
+                    break;
+                }
+                ++counter;
+                ++it;
+            }
+
+            if (warmer) {
+                result.push_back(counter);
+            } else {
+                result.push_back(0);
+            }
+        }
+        result.push_back(0);
+
+        return result;
+    }
+};
